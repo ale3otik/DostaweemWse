174c98c5769b05a03f21838a5e2ee2c084f53a13
sergmiller <sergey.miller@phystech.edu>
2017-12-10 21:59:55 +0300

    udpate route, order, graph

--- dostaweemvse/dostaweemvse/models/order.py
+++ dostaweemvse/dostaweemvse/models/order.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class Order(models.model):
-    route_id = models.ForeignKey(Route)
+    route = models.ForeignKey(Route)
     metadata = models.CharField(max_length=50)
-    from_id = models.ForeignKey(Location)
-    to_id = models.ForeignKey(Location)
+    from_location = models.ForeignKey(Location)
+    to_location = models.ForeignKey(Location)
+    max_cost = models.IntegerField()
