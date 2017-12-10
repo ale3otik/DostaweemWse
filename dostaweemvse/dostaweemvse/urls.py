832faadd7af07437600c5b708d2f08e52638f2eb
ale3otik <aleksey.zotov@phystech.edu>
2017-12-10 20:12:50 +0300

    add simple order_info page

--- dostaweemvse/dostaweemvse/urls.py
+++ dostaweemvse/dostaweemvse/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('', views.index, name='index'),
+    url('order_info/', views.order_info, name='order_info'),
+    url('index/', views.index, name='index'),
 ]
--- dostaweemvse/dostaweemvse/views.py
+++ dostaweemvse/dostaweemvse/views.py
@@ -1,3 +1,16 @@
 from django.http import HttpResponse
+from django.http import QueryDict
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("<b>Hello, world. You're at the index.<b/>" )
+
+def order_info(request):
+    query_dict = request.GET
+    try:
+        order_id = query_dict['order_id']
+    except:
+        order_id = None
+    
+    if order_id is None:
+        return HttpResponse('Please type-in correct order_id')    
+    return HttpResponse('order id is %s' %  order_id)
