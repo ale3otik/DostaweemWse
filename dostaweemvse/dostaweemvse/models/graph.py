dd7aa695f16bdcd90569b6d97cb573fad18de035
sergmiller <sergey.miller@phystech.edu>
2017-12-10 22:54:37 +0300

    pass->continue

--- dostaweemvse/dostaweemvse/models/graph.py
+++ dostaweemvse/dostaweemvse/models/graph.py
@@ -6,33 +6,33 @@ from .edge import Edge
 class Graph:
     @staticmethod
     def build_route(order):
-        path, sum_cost = Graph.find_best_path(order.from_location, order.to_location, order.weight)
+        path, sum_cost = Graph.find_best_path(order.from_location, order.to_location)
 
         if path is None or sum_cost > order.max_cost:
-            return None
+            order.route = None
         else:
-            return Route(edges=path, active_edge_index=0)
-            # route.save()
-            # order.route = route
-            # order.save()
+            route = Route(edges=path, active_edge_index=0)
+            route.save()
+            order.route = route
+            order.save()
 
     @staticmethod
-    def find_best_path(from_location, to_location, weight):
+    def find_best_path(from_location, to_location):
         best_path_to = {}
         dist_heap = [(0, from_location, None)]
 
         while len(dist_heap) > 0:
             cost, cur_location, best_last_edge = heappop(dist_heap)
             if cur_location.id in best_path_to:
-                pass
+                continue
             best_path_to[cur_location.id] = best_last_edge
             if cur_location == to_location:
                 break
             for edge in Edge.objects.filter(start_location=cur_location):
                 if edge.end_location.id in best_path_to:
                     continue
-                if edge.edge_type_id.max_weight >= weight:
-                    heappush(dist_heap, (cost + edge.cost(),
+                if edge.cost:
+                    heappush(dist_heap, (cost + edge.edge_type.cost,
                         edge.end_location, edge))
         else:
             return None, None
@@ -43,6 +43,6 @@ class Graph:
         while cur != from_location:
             edge = cur[cur.id]
             path.append(edge)
-            sum_cost += edge.cost
+            sum_cost += edge.edge_type.cost *
             cur = edge.start_location
         return reversed(path), sum_cost
