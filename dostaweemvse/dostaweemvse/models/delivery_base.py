ebbb4414edae43ee8e7c7377f0040da7649b286b
Nick Karpachev <n.karpachov@gmail.com>
2017-12-10 23:06:42 +0300

    Initial service and delivery_base

--- dostaweemvse/dostaweemvse/models/delivery_base.py
+++ dostaweemvse/dostaweemvse/models/delivery_base.py
@@ -1,11 +1,11 @@
 from django.db import models
+from .order import Order
 
 class DeliveryBase(models.model):
 	orders = models.ManyToManyField(Order)
 
 	def save_order(order):
-		self.orders.add(order)
-
+		orders.add(order)
 
 	def get_order(order_id):
-		return self.orders.all().get(pk=order_id)
+		return orders.all().get(pk=order_id)
---
+++ dostaweemvse/dostaweemvse/models/service.py
@@ -0,0 +1,48 @@
+from django.db import models
+from .delivery_base import DeliveryBase
+from .graph import Graph
+
+
+class Service(object):
+	def get_order_info(order_id):
+		self.delivery_base = DeliveryBase()
+
+		try:
+			order = self.delivery_base.get_order(order_id)
+		except Exception:
+			return None
+
+		order_info = {
+			'source': order.from_location,
+			'target': order.to_location,
+			'route': list(order.route.edges.all()),
+			'position': order.route.active_edge_index,
+			'weight': order.route.get_active_edge().edge_type_id.max_weight
+		}
+
+		return order_info
+
+	def make_order(data):
+		_from = data['source']
+		_to = data['target']
+		_route = None
+		_max_cost = data['weight']
+
+		if 'meta' in data.keys():
+			_metadata = data['meta']
+		else:
+			_metadata = ""
+	
+		_order = Order(_route, _metadata, _from, _to, _max_cost)
+		_route = build_route(_order)
+		
+		if _route:
+			_route.save()
+			_order.route = _route
+			_order.save()
+
+			self.delivery_base.save_order(_order)
+			self.delivery_base.save()
+
+		else:
+			raise RuntimeError()
