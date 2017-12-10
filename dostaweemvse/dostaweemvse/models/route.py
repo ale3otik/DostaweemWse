e2d448e5cf630e1c462632a320f9f94c75f0e10b
sergmiller <sergey.miller@phystech.edu>
2017-12-10 20:52:18 +0300

    add route

--- dostaweemvse/dostaweemvse/models/route.py
+++ dostaweemvse/dostaweemvse/models/route.py
@@ -1,2 +1,12 @@
-class Маршрут:
-    pass
+from django.db import models
+
+
+class Route(models.model):
+    edges = models.ManyToManyField(Edge, through="Index")
+    active_edge_index = models.IntegerField()
+
+    def go_to_next_edge():
+        active_edge_index += 1
+
+    def get_active_edge_id():
+        return edges.all()[active_edge_index]
