503dba3d5edf329fb70d1232e797ed955e475d2f
sergmiller <sergey.miller@phystech.edu>
2017-12-10 20:30:53 +0300

    remove edge type id

--- dostaweemvse/dostaweemvse/models/edge.py
+++ dostaweemvse/dostaweemvse/models/edge.py
@@ -2,13 +2,12 @@ from django.db import models
 
 
 class Edge(models.Model):
-    start_location = models.IntegerField()
-    end_location = models.IntegerField()
+    start_location_id = models.IntegerField()
+    end_location_id = models.IntegerField()
     edge_type_id = models.IntegerField()
     length = models.IntegerField()
 
 
 class TypeOfEdge(models.Model):
-    edge_type_id = models.IntegerField(primary_key=True)
     cost = models.IntegerField()
     name = models.CharField(max_length=30)
